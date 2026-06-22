6fb0fe419525d054b70e36128856e934781f0047
norbe98 <szoke.norbi18@gmail.com>
2026-06-22 18:22:38 +0200

    add prisma models, prisma config + server.ts updated version

---
+++ backend/prisma/migrations/20260619144027_init/migration.sql
@@ -0,0 +1,43 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Project" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "Project_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Task" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "projectId" INTEGER NOT NULL,
+    "description" TEXT NOT NULL,
+
+    CONSTRAINT "Task_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Project_name_key" ON "Project"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Task_name_key" ON "Task"("name");
+
+-- AddForeignKey
+ALTER TABLE "Project" ADD CONSTRAINT "Project_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Task" ADD CONSTRAINT "Task_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Project"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,13 @@
 import express from 'express'
 import cors from 'cors'
-import { AuthRouter } from './routes/auth.routes.js'
+import { authRouter } from './routes/auth.routes.js'
 
 const app = express()
 const PORT = 3000
+app.use(express.json())
 app.use(cors())
 
-app.use("/api/auth", AuthRouter)
+app.use("/api/auth", authRouter)
 
 app.listen(PORT, () => {
     console.log(`The app is running on http://localhost:${PORT}`)
